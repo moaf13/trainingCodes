a45268384cee18b1b9203c149a452ede6268f8a7
1260210 <1260210@40HW012175.NOAM.TCS.com>
2016-05-16 16:09:20 -0400

    last changes

--- JavaTest/src/test.java
+++ JavaTest/src/test.java
@@ -1,10 +1,14 @@
-import java.util.Scanner;
+import static java.lang.System.*;
 
 public class test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] __A_V_) {
+		String $ = "wow";
 		
+		for(int x=0; ++x < __A_V_.length;){
+			$ += __A_V_[x];
+			out.println($);
+		}
 	
 	}
 
